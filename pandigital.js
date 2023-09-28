8bed313ad7e4778e22a44e0a815b70e168548b6c
anthony1m <you@example.comdcdc>
2023-09-28 10:15:29 +0300

    pandigital game

---
+++ pandigital.js
@@ -0,0 +1,22 @@
+const input = prompt("Input a random number, but it should be pandigital: ");
+//thawwel l input yalli hiye for example "123" la ['1,2,3']la tsir array 
+const inputDigits = Array.from(input).map(Number);
+
+const isPandigital = (inputDigits) => {
+    //new set kermel ykouno l numbers unique bt hawel l array la set
+  const uniqueDigits = new Set(inputDigits);
+
+  
+  return (
+      //.size la2ano l set btekhod unique digits w .length la2ano size taba3 l array that we inputted fa bt chouf eza fi duplicate numbers 
+    uniqueDigits.size === inputDigits.length &&
+    //.from kermel terja3 la array wl .every kermel kermel tchayek kell digit la wahdo eza akbar mn 0 w azghar mn 9 
+    Array.from(uniqueDigits).every((digit) => digit >= 0 && digit <= 9)
+  );
+};
+
+if (isPandigital(inputDigits)) {
+  console.log("Your number is pandigital");
+} else {
+  console.log("Your number is not pandigital");
+}
